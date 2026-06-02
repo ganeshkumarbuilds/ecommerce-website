de40384071f458416e0fff1970201aabef0b9f7f
ganeshkumarbuilds <ganeshkumar.chavala@gmail.com>
2026-06-02 16:41:55 +0530

    Fix:update admin endpoints to live Render URL

--- admin/src/Components/AddProduct/AddProduct.jsx
+++ admin/src/Components/AddProduct/AddProduct.jsx
@@ -31,7 +31,7 @@ const AddProduct = () => {
         let formData = new FormData();
         formData.append('product',image);
 
-        await fetch('http://localhost:4000/upload',{
+        await fetch('https://ecommerce-website-backend-xmvr.onrender.com/upload',{
             method:'POST',
             headers:{
                 Accept:'application/json',
@@ -42,7 +42,7 @@ const AddProduct = () => {
         if(responseData.success){
             product.image = responseData.image_url;
             console.log(product);
-            await fetch('http://localhost:4000/addproduct',{
+            await fetch('https://ecommerce-website-backend-xmvr.onrender.com/addproduct',{
                 method:'POST',
                 headers:{
                     Accept:'application/json',
